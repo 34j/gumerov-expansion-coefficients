b9902eb03e641e3e06ef7c7b64ff4157179dec5f
34j <34j.github@proton.me>
2025-07-13 18:53:19 +0900

    chore: wip

--- src/gumerov_expansion_coefficients/main.py
+++ src/gumerov_expansion_coefficients/main.py
@@ -9,18 +9,21 @@ P = ParamSpec("P")
 T = TypeVar("T")
 
 
-def R(n: Array, m: Array, theta: Array, phi: Array, k: Array) -> Array:
+# (2.14)
+def R(n: Array, m: Array, kr: Array, theta: Array, phi: Array) -> Array:
     """Regular elementary solution of 3D Helmholtz equation."""
-    return spherical_jn(n, k) * sph_harm(n, m, theta, phi)
+    return spherical_jn(n, kr) * sph_harm(n, m, theta, phi)
 
 
-def S(n: Array, m: Array, theta: Array, phi: Array, k: Array) -> Array:
+def S(n: Array, m: Array, kr: Array, theta: Array, phi: Array) -> Array:
     """Singular elementary solution of 3D Helmholtz equation."""
-    return spherical_yn(n, k) * sph_harm(n, m, theta, phi)
+    return spherical_yn(n, kr) * sph_harm(n, m, theta, phi)
 
 
 # Gumerov's notation
 # E^m_n = sum_{m'n'} (E|F)^{m' m}_{n' n} F^{m'}_{n'}
+# (E|F)^{m' m}_{n'} := (E|F)^{m' m}_{n' |m|}
+# (E|F)^{m' m}_{,n} := (E|F)^{m' m}_{|m'| n}
 
 
 def idx(n: Array | int, m: Array | int, /) -> Array:
@@ -44,12 +47,43 @@ def ndim_harm(n_end: int, /) -> int:
     return n_end**2
 
 
-def translational_coefficients(k: Array, dr: Array, same: bool, n_end: int, /) -> Array:
-    xp = array_namespace(k, dr)
-    result = xp.zeros(
-        (ndim_harm(n_end), ndim_harm(n_end)), dtype=k.dtype, device=k.device
+def minus_1_power(x: Array, /) -> Array:
+    return 1 - 2 * (x % 2)
+
+
+def a(n: Array, m: Array, /) -> Array:
+    xp = array_namespace(n, m)
+    m_abs = xp.abs(m)
+    return xp.where(
+        m_abs > n,
+        0,
+        xp.sqrt((n + m_abs + 1) * (n - m_abs + 1) / ((2 * n + 1) * (2 * n + 3))),
     )
-    # (E|F)^{m' 0}_{n' 0}
+
+
+def b(n: Array, m: Array, /) -> Array:
+    xp = array_namespace(n, m)
+    return xp.where(
+        xp.abs(m) > n,
+        0,
+        xp.sqrt((n - m - 1) * (n - m) / ((2 * n - 1) * (2 * n + 1))) * xp.sign(n),
+    )
+
+
+def translational_coefficients(
+    kr: Array, theta: Array, phi: Array, same: bool, n_end: int, /
+) -> Array:
+    xp = array_namespace(kr, theta, phi)
+    result = xp.zeros(
+        (ndim_harm(n_end), ndim_harm(n_end)), dtype=kr.dtype, device=kr.device
+    )
+    n, m = idx_all(n_end, xp=xp)
+    # (E|F)^{m' 0}_{n' 0} = (E|F)^{m' 0}_{n'}
     if not same:
         # 4.43
-        result[:, idx(0, 0)] = xp.sqrt(4 * xp.pi)
+        result[:, idx(0, 0)] = xp.sqrt(4 * xp.pi) * S(n, -m, -kr, theta, phi)
+    else:
+        # 4.58
+        result[:, idx(0, 0)] = xp.sqrt(4 * xp.pi) * R(n, -m, kr, theta, phi)
+    # 4.67
+    # result
