110d4cb0766da4464faf828300fa4bf41658a798
34j <34j.github@proton.me>
2025-07-14 16:13:55 +0900

    chore: wip

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,19 @@
 from array_api._2024_12 import ArrayNamespaceFull
 
-from gumerov_expansion_coefficients.main import R, idx, idx_all
+from gumerov_expansion_coefficients.main import idx, idx_all, idx_i, minus_1_power, ndim_harm
 
 
-def test_idx() -> None:
-    assert idx(0, 0) == 0
-    assert idx(1, -1) == 1
-    assert idx(1, 0) == 2
-    assert idx(1, 1) == 3
+def test_idx(xp: ArrayNamespaceFull) -> None:
+    n = xp.asarray([0, 1, 1, 1])
+    m = xp.asarray([0, -1, 0, 1])
+    assert xp.all(idx(n, m) == xp.asarray([0, 1, 2, 3]))
+
+
+def test_idx_i() -> None:
+    assert idx_i(0, 0) == 0
+    assert idx_i(1, -1) == 1
+    assert idx_i(1, 0) == 2
+    assert idx_i(1, 1) == 3
 
 
 def test_idx_all(xp: ArrayNamespaceFull) -> None:
@@ -16,10 +22,13 @@ def test_idx_all(xp: ArrayNamespaceFull) -> None:
     assert xp.all(m == xp.asarray([0, -1, 0, 1, -2, -1, 0, 1, 2]))
 
 
-def test_R(xp: ArrayNamespaceFull) -> None:
-    m, n = idx_all(3, xp=xp)
-    theta = xp.linspace(0, xp.pi, 10)
-    phi = xp.linspace(0, 2 * xp.pi, 10)
-    k = xp.ones_like(theta)
-    result = R(n, m, theta[:, None], phi[None, :], k)
-    assert result.shape == (10, 10, 9)  # 10 theta
+def test_ndim_harm() -> None:
+    assert ndim_harm(2) == 4
+
+
+def test_minus_1_power() -> None:
+    assert minus_1_power(0) == 1
+    assert minus_1_power(1) == -1
+    assert minus_1_power(2) == 1
+    assert minus_1_power(3) == -1
+    assert minus_1_power(4) == 1
